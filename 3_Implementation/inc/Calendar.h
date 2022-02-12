2b7cef5b7b6a563773e9984da96d5f1557ffd94a
Mohmad Aymen Naqash <62838172+aymen2106@users.noreply.github.com>
2022-02-12 16:42:05 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Calendar.h
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+COORD xy = {0, 0};
+
+void gotoxy (int x, int y)//This func allows you to print text in any place of screen.//
+{
+        xy.X = x; xy.Y = y; // X and Y coordinates
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
+}
+void SetColor(int ForgC)
+{
+     WORD wColor;
+    
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+          SetConsoleTextAttribute(hStdOut, wColor);
+     }
+     return;
+void ClearColor(){
+    SetColor(15);
+}
+
+void ClearConsoleToColors(int ForgC, int BackC)
+{
+     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     COORD coord = {0, 0};
+
+     DWORD count;
+
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+     SetConsoleTextAttribute(hStdOut, wColor);
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+          FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+
+          FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
+          SetConsoleCursorPosition(hStdOut, coord);
+     }
+     return;
+
